1aba94dd1daf9384bf6212dde4dc43e68798bc98
pc1 <pc1@DESKTOP-GOII4U4>
2019-03-06 10:41:08 +0200

    Tipul Fractie

---
+++ Problema/Problema/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema
+{
+    class Program
+    {
+        static int Apa(int[] a)
+        {
+            int litrii = 0;
+            int min = 0;
+           for(int i=0;i<a.Length;i++)
+                if (a[i] > 0) min = a[i];
+                else 
+                if (min - a[i] > 0)
+                    litrii = litrii + min - a[i];
+
+            
+       
+         
+            return litrii;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 0, 3, 0, 0, 7, 4, 1, 0, 3 };
+            Console.WriteLine(Apa(a));
+            Console.ReadKey();
+
+        }
+    }
+}
