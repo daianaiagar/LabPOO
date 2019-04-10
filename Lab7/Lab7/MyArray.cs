fdbc85b5737c62d1d947145e1eb283127dd25cc6
veres roland <rolandveres@yahoo.com>
2019-04-10 09:44:10 +0300

    dfsdfsdfsdf

---
+++ Lab7/Lab7/MyArray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class MyArray
+    {
+        private int[] arr;
+        private int size; //numarul de elemente din vector.
+        public MyArray(int capacity)
+        {
+            arr = new int[capacity];
+            size = 0;
+
+        }
+
+        public  void Add(int value)
+        {
+            if (size < Capacity)
+            {
+                arr[size] = value;
+                size++;
+            }
+               //exceptie.Dublam capacitatea.
+
+
+            
+                
+            
+        }
+        public int this[int i]
+        {
+            get {
+                return arr[i];
+            }
+            set {
+                arr[i] = value;
+            }
+        }
+
+        public int Capacity
+        {
+            get { return arr.Length; }
+        }
+        public override string ToString()
+        {
+            // return $"({string.Join(",",arr)})";
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+              foreach(var item in arr)
+            {
+                sb.Append(item);
+                sb.Append(",");
+            }
+            sb.Remove(sb.Length - 2, 2);
+            sb.Append("]");
+            return sb.ToString();
+
+        }
+
+    }
+}
---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.Title = "Laborator7";
+            MyArray mA = new MyArray(10);
+            for (int i = 0; i <10; i++)
+                mA.Add(i * i);
+            mA[3] += 5;
+            Console.WriteLine(mA);
+            //toate aparitiile
+           // mA.Remove(14);
+
+                        
+           
+
+        }
+    }
+}
